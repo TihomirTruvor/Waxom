9d744783b06b3e72b667eb9dec60688424ddeb42
TihomirTruvor <tihomir.truvor@mail.ru>
2020-06-05 01:11:24 +0500

    Search Review №1

---
+++ js/search.js
@@ -0,0 +1,10 @@
+let search = document.getElementById('main-search').onclick = function() {
+	let searchText = document.getElementById('search-text');
+	searchText.style.display = 'block'
+
+	let searchButton = document.getElementById('search-button');
+	searchButton.style.display = 'block'
+
+	let searchButtonHead = document.getElementById('search-button-head');
+	searchButtonHead.style.background = 'none';
+}
