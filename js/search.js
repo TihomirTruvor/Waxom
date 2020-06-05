46a735c301ff1471b2471e7cae0be04ed2ab1ef0
TihomirTruvor <tihomir.truvor@mail.ru>
2020-06-06 01:05:54 +0500

    Review search №2

--- js/search.js
+++ js/search.js
@@ -1,10 +1,44 @@
-let search = document.getElementById('main-search').onclick = function() {
+document.getElementById('main-search').onclick = function() { // функция скрыть и показать поиск
+	
 	let searchText = document.getElementById('search-text');
-	searchText.style.display = 'block'
+	searchText.style.display = 'block' // Открываем поле ввода
 
 	let searchButton = document.getElementById('search-button');
-	searchButton.style.display = 'block'
+	searchButton.style.display = 'block' // Открываем кнопку поиска
 
 	let searchButtonHead = document.getElementById('search-button-head');
-	searchButtonHead.style.background = 'none';
-}
+	searchButtonHead.style.background = 'none'; // Скрываем кнопку в основном меню
+};
+
+$(document).ready(function(){ // функция поиска на странице
+
+  $("#search-button").click(function(){ // Обрабатка нажатия на кнопку
+
+    // Очищаем переменную, в которой будет поисковый запрос
+    var text = "";
+ 
+    // Убираем всю подсветку из прошлого поиска
+    $('body').removeHighlight();
+
+    // Берем текст из поля ввода и кладем в переменную text
+    text = $('#search-text').attr('value');
+
+    // Если строка поиска пустая
+    if ($('#search-text').val() == ""){
+
+      //То ничего не происходит ;)
+
+      return false;
+
+     // Иначе
+    } else {
+
+      //Подсвечиваем все найденные совпадения
+      $('body').highlight(text);
+
+      return false;
+
+    }
+  });      
+});
+
